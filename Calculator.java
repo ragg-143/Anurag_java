a18b20fec946b367a6b575b953b3841c27d8d1d9
ragg-143 <110332618+ragg-143@users.noreply.github.com>
2022-10-02 16:17:27 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+class Calculator{
+  public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter 1 to add,2to subtract);
+   int t1,t2;
+                       t1=s.nextInt();
+                       t2=s.nextInt();
+                       int option=s.nextInt();
+                       switch(option);
+                       {
+                         case 1:System.out.println(t1+t2);
+                         break;
+                         case 2:System.out.println(t1-t2);
+                         break;
